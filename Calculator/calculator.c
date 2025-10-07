242ee37567d0c1febfbcade076d2f5c0c9d40d4e
ShubhamProgr <sbahugunacode@gmail.com>
2025-10-07 22:50:42 +0530

    Optimized Advanced Section

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -17,32 +17,37 @@ switch (mode) {
     case 1:
         printf("\n Basic Arithmetic Mode ");
         printf("-------------------------------------\n");
-        printf("A. Addition (+)\n");
-        printf("B. Subtraction (-)\n");
-        printf("C. Multiplication (x)\n");
-        printf("D. Division (/)\n");
+        printf("A. Addition\n");
+        printf("B. Subtraction\n");
+        printf("C. Multiplication\n");
+        printf("D. Division\n");
         printf("E. Power\n");
         printf("F. Remainder\n");
 
         printf("\nEnter your choice : ");
         scanf(" %c", &operator);
 
-        printf("Enter two numbers: ");
-        scanf("%lf %lf", &num1, &num2);
+        printf("Enter first operand : ");
+        scanf("%lf", &num1);
+        printf("Enter second operand : ");
+        scanf("%lf",&num2);
 
         switch (operator) {
             case 'A':
             case 'a':
                 printf("Result: %.2lf", num1 + num2);
                 break;
+
             case 'B':
             case 'b':
                 printf("Result: %.2lf", num1 - num2);
                 break;
+
             case 'C':
             case 'c':
                 printf("Result: %.2lf", num1 * num2);
                 break;
+
             case 'D':
             case 'd':
                 if (num2 != 0) {
@@ -51,10 +56,12 @@ switch (mode) {
                     printf("Error! Division by zero.");
                 }
                 break;
+
             case 'E':
             case 'e':
                 printf("Result: %.2lf", pow(num1, num2));
                 break;
+
             case 'F':
             case 'f':
                 if (num2 != 0) {
@@ -63,6 +70,7 @@ switch (mode) {
                     printf("Error! Division by zero.");
                 }
                 break;
+
             default:
                 printf("Error! Invalid operator for this mode.");
                 break;
@@ -70,20 +78,32 @@ switch (mode) {
     
     case 2:
         printf("\nAdvanced Operations Mode");
-        printf("-------------------------------------\n");
+        printf("\n-------------------------------------");
         printf("A. Square Root\n");
         printf("B. Factorial\n");
         
         printf("\nEnter your choice : ");
         scanf(" %c", &operator);
 
-        printf("Enter two numbers: ");
-        scanf("%lf %lf", &num1, &num2);
         switch (operator) {
-            
+            case 'A':
+            case 'a':
+                printf("Enter a number: ");
+                scanf("%lf", &num1);
+                if (num1 >= 0) {
+                    printf("Result: %.2lf", sqrt(num1));
+                } else {
+                    printf("Error! Square root of negative number.");
+                }
+                break;
+
+            case 'B':
+            case 'b': 
+             
             default:
                 printf("Error! Invalid operator for this mode.");
                 break;
+
             }
 default:
     printf("Error! Invalid operator choice.");
